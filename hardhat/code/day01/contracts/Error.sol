2a407e319a64d4eb4c904c502f76c9e9c50e4862
shiqianava <1085503167@qq.com>
2022-04-12 11:35:53 +0800

    4.12中午

---
+++ hardhat/code/day01/contracts/Error.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/* 
+Error
+An error will undo all changes made to the state during a transaction.
+You can throw an error by calling require, revert or assert.
+1. require is used to validate inputs and conditions before execution.
+2. revert is similar to require. See the code below for details.
+3. assert is used to check for code that should never be false. Failing assertion probably means that there is bug.
+*/
