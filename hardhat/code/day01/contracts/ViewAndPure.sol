e103cc531694ac52c144ae94de7801eb268e2aa5
Sqava <1085503167@qq.com>
2022-06-06 11:26:08 +0800

    22.6.6上午

--- hardhat/code/day01/contracts/ViewAndPure.sol
+++ hardhat/code/day01/contracts/ViewAndPure.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+/*
+函数修饰符-view
+* 表示一个函数不能修改状态，view 和 constant 等价，constant 在0.5版本后会弃用
+* 本地执行不消耗 gas
+* 下面几种情况认为是修改了状态：
+    1.写状态变量  2.触发事件（events）  3.创建其他的合约  4.call调用附加了以太币
+    5.调用了任何没有view或pure修饰的函数 6.使用了低级别的调用（low-level calls）
+*/
+/*
+函数修饰符-pure
+* 表示一个函数不读取状态，也不修改状态
+* 下面几种情况认为是读取了状态：
+    1.读状态变量  2.访问了.balance属性  
+    3.访问了block、tx、msg成员（msg.sig 和 msg.data除外）
+    4.调用了任何没有pure修饰的函数
+*/
+
 contract ViewAndPure {
     uint x = 1;
 
--- hardhat/code/day01/contracts/Visibility.sol
+++ hardhat/code/day01/contracts/Visibility.sol
@@ -12,11 +12,64 @@ external - 只有其他合约和账户可以调用
 */
 
 contract Base {
-    // 私有函数只能在函数内部被调用
+    // private私有函数
+    // 只能在函数内部被调用
     // 继承该合约的合约不能调用
     function privateFunc() private pure returns (string memory) {
         return "private function called";
     }
 
-    function testPrivateFunc() public
+    function testPrivateFunc() public pure returns (string memory) {
+        return privateFunc();
+    }
+    
+    // internal内部函数
+    // 可以在合约内部调用
+    // 可以再继承合约中调用
+    function internalFunc() internal pure returns (string memory) {
+        return "internal function called";
+    }
+
+    function testInternalFunc() public pure virtual returns (string memory) {
+        return internalFunc();
+    }
+
+    // public公有函数
+    // 合约内部可调用
+    // 继承的合约可调用
+    // 其他合约和账户可调用
+    function publicFunc() public pure returns (string memory) {
+        return "public function called";
+    }
+
+    // external外部函数
+    // 只能通过其他函数和合约调用
+    function externalFunc() external pure returns (string memory) {
+        return "external function called";
+    } 
+
+    // 如果我们尝试在此调用外部函数，函数将不能编译
+    // function testExternalFunc() public pure returns (string memory) {
+    //     return externalFunc();
+    // }
+
+    // 状态变量
+    string private privateVar = "my private variable";
+    string internal internalVar= "my internal variable";
+    string public publicVar = "my public variable";
+    // 状态变量不能定义为external，所以这段代码不能编译
+    // string external externalVar = "my external variable";
+}
+
+contract Child is Base {
+    // 继承合约不能访问私有函数和变量
+    // function testPrivateFunc() public pure returns (string memory) {
+    //     return privateFunc();
+    // }
+
+    // 内部函数可以再子合约中调用
+    // 子合约重写父合约函数，父合约函数必须定义为virtual
+    function testInternalFunc() public pure override returns (string memory) {
+        return internalFunc();
+    }
 }
