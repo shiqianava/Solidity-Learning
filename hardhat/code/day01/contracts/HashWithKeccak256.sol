633925dd958b464ea35a5dca98107a14e5429188
Sqava <1085503167@qq.com>
2022-06-22 11:25:36 +0800

    22.6.22上午

--- hardhat/code/day01/contracts/HashWithKeccak256.sol
+++ hardhat/code/day01/contracts/HashWithKeccak256.sol
@@ -8,4 +8,25 @@ keccak256算法计算"输入"的 Keccak-256 哈希值
 2. Commit-Reveal scheme（提交-释放 方案）
 3. 压缩密码签名（通过压缩后的哈希值来签名，替代大的输入）
 */
-contract
+contract HashFunction {
+    function hash(
+        string memory _test,
+        uint _num,
+        address _addr
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_test, _num, _addr));
+    }
+
+    // hash冲突的例子
+    // 当传递多个动态数据类型到abi.encodePacked()，可能会发生hash冲突
+    // 在这个例子中，应该使用abi.encode()来代替
+    function collision(string memory _text, string memory _anotherText)
+        public
+        pure
+        returns (bytes32) 
+    {
+        // encodePacked(AAA, BBB) -> AAABBB
+        // necodepacked(AA, ABBB) -> AAABBB
+        return keccak256(abi.encodePacked(_text, _anotherText);)
+    }
+}
