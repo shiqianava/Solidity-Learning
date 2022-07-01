7fa3e1e879d2fbc28d921eed01c1fb8522a4beea
Sqava <1085503167@qq.com>
2022-07-01 17:09:02 +0800

    2022.7.1

--- hardhat/code/day01/contracts/VerifyingSignature.sol
+++ hardhat/code/day01/contracts/VerifyingSignature.sol
@@ -40,11 +40,39 @@ contract VerifySignature {
         return keccak256(abi.encodePacked(_to, _amount, _message, _nonce));
     }
 
-    /* 3.签名消息哈希
+    /* 3.签名消息后得到的哈希值
     # 使用浏览器
     account = "在此处复制粘贴签名者的账户"
     ethereum.request({ method: "personal_sign", params: [account, hash]}).then(console.log)
+
     # 使用 web3
-    web3.personal.sign(hash, web3.eth)
+    web3.personal.sign(hash, web3.eth.defaultAccount, console.log)
+
+    不同账户的签名不同
+    0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b
+    */
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        public
+        pure
+        returns (bytes32) 
+    {
+        /* 签名是通过使用以下格式，对keccak256散列值签名生成的：
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        len(msg)的值是 32byte
+        */
+        return
+        keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+        );
+    }
+
+    /* 4. Verify signature
+    signer = 0xB273216C05A8c0D4F0a4Dd0d7Bae1D2EfFE636dd
+    to = 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
+    amount = 123
+    message = "coffee and donuts"
+    nonce = 1
+    signature =
+        0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b
     */
 }
